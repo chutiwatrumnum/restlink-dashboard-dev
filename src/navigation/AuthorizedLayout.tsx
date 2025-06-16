c8f927d5cda84c1494bd1451166fc0d9a743d1d6
chutiwatrumnum <chutiwatrumnum@gmail.com>
2025-06-16 12:46:45 +0700

    update navbar

--- src/navigation/AuthorizedLayout.tsx
+++ src/navigation/AuthorizedLayout.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useOutlet } from "react-router-dom";
 import { Layout } from "antd";
 import { useSelector } from "react-redux";
 import { RootState } from "../stores";
-// import { useDispatch } from "react-redux";
-// import { Dispatch } from "../stores";
-// import { encryptStorage } from "../utils/encryptStorage";
-
 import SideMenu from "../components/templates/SideMenu";
-
 import "./styles/authorizedLayout.css";
 
 const { Sider, Content } = Layout;
@@ -17,19 +12,53 @@ function AuthorizedLayout() {
   const { isAuth } = useSelector((state: RootState) => state.userAuth);
   const outlet = useOutlet();
 
+  const [collapsed, setCollapsed] = useState(() => {
+    const savedState = localStorage.getItem("sideMenuCollapsed");
+    return savedState === "true";
+  });
+
+  // Listen for menu collapse changes
+  useEffect(() => {
+    const handleCollapsedChange = () => {
+      const newCollapsedState =
+        localStorage.getItem("sideMenuCollapsed") === "true";
+      setCollapsed(newCollapsedState);
+    };
+
+    window.addEventListener("sideMenuCollapsed", handleCollapsedChange);
+    return () => {
+      window.removeEventListener("sideMenuCollapsed", handleCollapsedChange);
+    };
+  }, []);
+
   useEffect(() => {
     if (!isAuth && window.location.pathname !== "/auth") {
       window.location.pathname = "/auth";
     }
   }, [isAuth]);
 
+  const siderWidth = collapsed ? 80 : 320;
+
   return (
     <Layout>
-      <Sider width={320} className="sideContainer">
+      <Sider
+        width={320}
+        collapsedWidth={80}
+        collapsed={collapsed}
+        trigger={null}
+        className="sideContainer"
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}>
         <SideMenu />
       </Sider>
-      <div className="authorizeBG" />
-      <Layout style={{ marginLeft: 320 }}>
+      <div className="authorizeBG" style={{ left: siderWidth }} />
+      <Layout style={{ marginLeft: siderWidth, transition: "all 0.3s" }}>
         <Content className="authorizeContentContainer">
           <div>{outlet}</div>
         </Content>
