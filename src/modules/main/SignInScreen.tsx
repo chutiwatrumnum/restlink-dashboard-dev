1e7388b143e4b9e5a7626acd324fdb43f1ccab04
chutiwatrumnum <chutiwatrumnum@gmail.com>
2025-06-23 18:12:36 +0700

    update

--- src/modules/main/SignInScreen.tsx
+++ src/modules/main/SignInScreen.tsx
@@ -70,8 +70,8 @@ const SignInScreen = () => {
               <div className="logo-container">
                 <img
                   src={LOGO}
-                  alt="Central Pattana Residence"
-                  className="central-pattana-logo"
+                  alt="Logo Brand"
+                  className="logo-brand"
                 />
               </div>
 
@@ -90,34 +90,6 @@ const SignInScreen = () => {
               // onFinishFailed={onFinishFailed}
               autoComplete="off">
               {/* Google Sign In Button */}
-              {/* <Button
-              size="large"
-              block
-              className="google-signin-button"
-              icon={
-                <svg width="18" height="18" viewBox="0 0 18 18">
-                  <path
-                    fill="#4285F4"
-                    d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 0 0 2.38-5.88c0-.57-.05-.66-.15-1.18z"
-                  />
-                  <path
-                    fill="#34A853"
-                    d="M8.98 16c2.16 0 3.97-.72 5.3-1.94l-2.6-2.04a4.8 4.8 0 0 1-7.18-2.53H1.83v2.07A8 8 0 0 0 8.98 16z"
-                  />
-                  <path
-                    fill="#EA4335"
-                    d="M4.5 9.49H1.83V7.42h2.68A8.09 8.09 0 0 0 8.98 0c2.16 0 3.97.72 5.3 1.94l-2.6 2.04a4.8 4.8 0 0 1-7.18 2.53z"
-                  />
-                  <path
-                    fill="#FBBC05"
-                    d="M8.98 3.58c1.22 0 2.32.42 3.18 1.24l2.39-2.4A7.81 7.81 0 0 0 8.98 0a8 8 0 0 0-7.15 4.42l2.67 2.07c.83-1.23 2.07-2.91 4.48-2.91z"
-                  />
-                </svg>
-              }>
-              Sign in with Google
-            </Button> */}
-
-              {/* Divider */}
               <Button
                 onClick={handleLogin}
                 type="primary"
@@ -131,7 +103,6 @@ const SignInScreen = () => {
               <div className="signin-divider">
                 <span>Or sign in with email</span>
               </div>
-
               {/* Email Input */}
               <Form.Item
                 name="username"
@@ -143,7 +114,6 @@ const SignInScreen = () => {
                   className="modern-input"
                 />
               </Form.Item>
-
               {/* Password Input */}
               <Form.Item
                 name="password"
@@ -212,8 +182,8 @@ const SignInScreen = () => {
             <div className="main-text">
               <img
                 src={LOGO}
-                alt="Central Pattana Residence"
-                className="central-pattana-logo"
+                alt="Logo Brand"
+                className="logo-brand"
               />
             </div>
           </div>
